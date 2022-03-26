cd0ea890a0a25e5f2d623c9fabe1e84216d68560
Obi1Ken0bi <puzikov.artem@gmail.com>
2022-03-26 12:28:12 +0300

    net to json

--- src/main/java/ru/puzikov/neuralNetwork/NeuralNetwork.java
+++ src/main/java/ru/puzikov/neuralNetwork/NeuralNetwork.java
@@ -11,30 +11,30 @@ import java.io.IOException;
 
 public class NeuralNetwork {
     public static void main(String[] args) {
-        double[][] inputs = new double[100][];
+        double[][] inputs = new double[1000][];
 
 
-        double[][] outputs = new double[100][];
+        double[][] outputs = new double[1000][];
 
-        for (int i = 0; i < 100; i++) {
-            inputs[i] = new double[]{i * 10};
-            outputs[i] = new double[]{2 * i * 10};
+        for (int i = 0; i < inputs.length; i++) {
+            inputs[i] = new double[]{i};
+            outputs[i] = new double[]{Math.sqrt(i)};
         }
         ActivationFunction activationFunction = new SigmoidFunction();
         ActivationFunction linearFunction = new LinearFunction();
-        LayerNetwork network = new LayerNetwork(new int[]{5, 1},
+        LayerNetwork network = new LayerNetwork(new int[]{15, 1},
                 inputs[0].length,
                 new ActivationFunction[]{linearFunction, linearFunction});
-        network.train(700000, 0.000000000001, inputs, outputs);
+        network.train(100000, 0.000000000001, inputs, outputs);
         for (int i = 0; i < inputs.length; i++) {
             network.setInputLayer(inputs[i]);
             network.compute();
             System.out.println("Output: " + network.getOutput()[0] + " Expected: " + outputs[i][0]);
         }
-        network.setInputLayer(new double[]{3000});
+        network.setInputLayer(new double[]{16});
         network.compute();
         System.out.println(network.getOutput()[0]);
-        network.setInputLayer(new double[]{-3000});
+        network.setInputLayer(new double[]{49});
         network.compute();
         System.out.println(network.getOutput()[0]);
         ObjectMapper jsonMapper = new JsonMapper();
