b366c56ba22871e9da8ab05ef9f7b45e39eedeb4
ubik33 <puzikov.artem@gmail.com>
2022-04-22 22:16:51 +0300

    main2

--- src/main/java/ru/puzikov/neuralNetwork/NeuralNetwork.java
+++ src/main/java/ru/puzikov/neuralNetwork/NeuralNetwork.java
@@ -34,4 +34,158 @@ public class NeuralNetwork {
             System.out.println("Output: " + network.getOutput()[0] + " Expected: " + outputs[i][0]);
         }
     }
+    private static void pictureProcessing() {
+        double[][] outputs = new double[][]{
+                new double[]{0, 0, 1},
+                new double[]{0, 0, 1},
+                new double[]{1, 0, 0},
+                new double[]{1, 0, 0},
+                new double[]{1, 0, 0},
+                new double[]{1, 0, 0},
+                new double[]{1, 0, 0},
+                new double[]{0, 1, 0},
+                new double[]{0, 1, 0},
+                new double[]{0, 1, 0},
+                new double[]{0, 1, 0},
+                new double[]{0, 1, 0}
+        };
+
+
+        double[][] inputs = new double[][]{
+                new double[]{
+                        0, 0, 1, 1, 1,
+                        0, 0, 0, 1, 0,
+                        0, 0, 1, 0, 0,
+                        0, 1, 0, 0, 0,
+                        1, 1, 1, 1, 0
+                },
+                new double[]{
+                        0, 1, 1, 1, 1,
+                        0, 0, 0, 1, 0,
+                        0, 0, 1, 0, 0,
+                        0, 1, 0, 0, 0,
+                        1, 1, 1, 1, 1
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        1, 1, 1, 1, 1
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        1, 1, 1, 1, 1,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 1, 1, 1
+                },
+                new double[]{
+                        1, 1, 1, 1, 1,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        1, 1, 1, 1, 1,
+                        1, 1, 1, 1, 1,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        0, 1, 0, 0, 0,
+                        1, 1, 1, 0, 0,
+                        0, 1, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0,
+                        0, 1, 0, 0, 0,
+                        1, 1, 1, 0, 0,
+                        0, 1, 0, 0, 0
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 1, 0, 0, 0,
+                        1, 1, 1, 0, 0,
+                        0, 1, 0, 0, 0,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        0, 0, 0, 0, 0,
+                        0, 0, 1, 0, 0,
+                        0, 1, 1, 1, 0,
+                        0, 0, 1, 0, 0,
+                        0, 0, 0, 0, 0
+                },
+                new double[]{
+                        0, 0, 1, 0, 0,
+                        0, 0, 1, 0, 0,
+                        1, 1, 1, 1, 1,
+                        0, 0, 1, 0, 0,
+                        0, 0, 1, 0, 0
+                }
+        };
+
+
+        ActivationFunction activationFunction = new Sigmoid();
+        ActivationFunction linearFunction = new LinearFunction();
+        ActivationFunction tanh = new TanhFunction();
+        LayerNetwork network = new LayerNetwork(new int[]{15, 3},
+                inputs[0].length,
+                new ActivationFunction[]{tanh, activationFunction});
+        network.train(50000, 0.1, inputs, outputs);
+        for (int i = 0; i < inputs.length; i++) {
+            network.setInputLayer(inputs[i]);
+            network.compute();
+            System.out.println("Output: " + Arrays.toString(network.getOutput()) + " Expected: " + Arrays.toString(outputs[i]));
+        }
+        network.setInputLayer(new double[]{
+                0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0,
+                0, 0, 0, 1, 0,
+                0, 0, 1, 1, 1,
+                0, 0, 0, 1, 0
+        });
+        network.compute();
+        System.out.println(Arrays.toString(network.getOutput()));
+        network.setInputLayer(new double[]{
+                0, 0, 1, 0, 0,
+                0, 0, 1, 0, 0,
+                1, 1, 1, 1, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 1, 0, 0
+        });
+        network.compute();
+        System.out.println(Arrays.toString(network.getOutput()));
+        network.setInputLayer(new double[]{
+                0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0,
+                1, 1, 1, 1, 0,
+                0, 0, 0, 0, 0
+        });
+        network.compute();
+        System.out.println(Arrays.toString(network.getOutput()));
+        network.setInputLayer(new double[]{
+                0, 1, 1, 1, 1,
+                0, 0, 0, 1, 0,
+                0, 0, 1, 0, 0,
+                0, 0.7, 0, 0, 0,
+                1, 1, 1, 1, 1
+        });
+        network.compute();
+        System.out.println(Arrays.toString(network.getOutput()));
+    }
 }
